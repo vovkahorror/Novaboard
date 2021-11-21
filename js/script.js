7e03a520130d9d217eb427e7cc05319d8b5a95b0
Volodymyr Yaremchak <vovkahorror@gmail.com>
2021-11-22 01:55:30 +0200

    0.4

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".js-title__slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4500,
+    dots: true,
+    arrows: true,
+    fade: true,
+  });
+});
